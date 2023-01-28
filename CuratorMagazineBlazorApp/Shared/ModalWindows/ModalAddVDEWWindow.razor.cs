9ca6d9a3b31aa14abac43f3ed74c7672e254300c
Заид Мингалиев <MingalievZZ@stud.kai.ru>
2023-01-28 11:00:24 +0300

    Fix

--- CuratorMagazineBlazorApp/Shared/ModalWindows/ModalAddVDEWWindow.razor.cs
+++ CuratorMagazineBlazorApp/Shared/ModalWindows/ModalAddVDEWWindow.razor.cs
@@ -1,4 +1,4 @@
-﻿ using CuratorMagazineBlazorApp.Data.Services;
+﻿using CuratorMagazineBlazorApp.Data.Services;
 using CuratorMagazineWebAPI.Models.Entities.Domains;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
@@ -6,16 +6,17 @@ using Newtonsoft.Json;
 
 namespace CuratorMagazineBlazorApp.Shared.ModalWindows;
 
+/// <summary>
+/// Class ModalAddVDEWWindow.
+/// Implements the <see cref="ComponentBase" />
+/// </summary>
+/// <seealso cref="ComponentBase" />
 public partial class ModalAddVDEWWindow
 {
-    private User _vdew = new();
-
     /// <summary>
-    /// Gets or sets the role callback.
+    /// The vdew
     /// </summary>
-    /// <value>The role callback.</value>
-    [Parameter]
-    public EventCallback<User> RoleCallback { get; set; }
+    private User _vdew = new();
 
     /// <summary>
     /// Gets or sets the user service.
@@ -25,15 +26,39 @@ public partial class ModalAddVDEWWindow
     public UserService? UserService { get; set; }
 
     /// <summary>
+    /// Gets or sets the division service.
+    /// </summary>
+    /// <value>The division service.</value>
+    [Inject]
+    public DivisionService? DivisionService { get; set; }
+
+    /// <summary>
     /// Gets or sets the navigation manager.
     /// </summary>
     /// <value>The navigation manager.</value>
     [Inject]
-    public NavigationManager NavigationManager { get; set; } = null!;
+    public NavigationManager? NavigationManager { get; set; }
 
+    /// <summary>
+    /// The divisions
+    /// </summary>
     private List<Division>? _divisions = new();
 
-    private string _selectedDivision { get; set; }
+    /// <summary>
+    /// Gets or sets the selected division.
+    /// </summary>
+    /// <value>The selected division.</value>
+    private string? SelectedDivision { get; set; }
+
+    /// <summary>
+    /// On initialized as an asynchronous operation.
+    /// </summary>
+    /// <returns>A Task representing the asynchronous operation.</returns>
+    protected override async Task OnInitializedAsync()
+    {
+        var ret = await DivisionService?.PostAsync()!;
+        _divisions = JsonConvert.DeserializeObject<List<Division>>(ret.Result.Items?.ToString() ?? string.Empty);
+    }
 
     /// <summary>
     /// Called when [finish].
@@ -51,11 +76,13 @@ public partial class ModalAddVDEWWindow
     private void OnFinishFailed(EditContext editContext)
     {
         Console.WriteLine($"Failed: {JsonConvert.SerializeObject(_vdew)}");
-
     }
 
-    public void AddVDEW()
+    /// <summary>
+    /// Adds the vdew.
+    /// </summary>
+    public async void AddVDEWAsync()
     {
-
+        await UserService?.CreateAsync(_vdew)!;
     }
 }
--- CuratorMagazineBlazorApp/Shared/ModalWindows/ModalChangeVDEWWindow.razor.cs
+++ CuratorMagazineBlazorApp/Shared/ModalWindows/ModalChangeVDEWWindow.razor.cs
@@ -4,43 +4,63 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Newtonsoft.Json;
 
+namespace CuratorMagazineBlazorApp.Shared.ModalWindows;
 
-namespace CuratorMagazineBlazorApp.Shared.ModalWindows
+/// <summary>
+/// Class ModalChangeVDEWWindow.
+/// Implements the <see cref="ComponentBase" />
+/// </summary>
+/// <seealso cref="ComponentBase" />
+public partial class ModalChangeVDEWWindow
 {
-    public partial class ModalChangeVDEWWindow
+    private User? User { get; set; } = new User();
+
+    /// <summary>
+    /// Gets or sets the division service.
+    /// </summary>
+    /// <value>The division service.</value>
+    [Inject]
+    public DivisionService? DivisionService { get; set; }
+
+    /// <summary>
+    /// The divisions
+    /// </summary>
+    private List<Division>? _divisions = new();
+
+    /// <summary>
+    /// Called when [finish].
+    /// </summary>
+    /// <param name="editContext">The edit context.</param>
+    private void OnFinish(EditContext editContext)
+    {
+        Console.WriteLine($"Success: {JsonConvert.SerializeObject(User)}");
+    }
+
+    /// <summary>
+    /// Called when [finish failed].
+    /// </summary>
+    /// <param name="editContext">The edit context.</param>
+    private void OnFinishFailed(EditContext editContext)
+    {
+        Console.WriteLine($"Failed: {JsonConvert.SerializeObject(User)}");
+
+    }
+
+    /// <summary>
+    /// On initialized as an asynchronous operation.
+    /// </summary>
+    /// <returns>A Task representing the asynchronous operation.</returns>
+    protected override async Task OnInitializedAsync()
+    {
+        var ret = await DivisionService?.PostAsync()!;
+        _divisions = JsonConvert.DeserializeObject<List<Division>>(ret.Result.Items?.ToString() ?? string.Empty);
+    }
+
+    /// <summary>
+    /// Changes the vdew.
+    /// </summary>
+    public void ChangeVDEW()
     {
 
-        [Parameter]
-        public User? _vdew { get; set; }
-
-        private List<Division>? _divisions = new();
-
-
-        [Parameter]
-        public EventCallback<User> RoleCallback { get; set; }
-
-        /// <summary>
-        /// Called when [finish].
-        /// </summary>
-        /// <param name="editContext">The edit context.</param>
-        private void OnFinish(EditContext editContext)
-        {
-            Console.WriteLine($"Success: {JsonConvert.SerializeObject(_vdew)}");
-        }
-
-        /// <summary>
-        /// Called when [finish failed].
-        /// </summary>
-        /// <param name="editContext">The edit context.</param>
-        private void OnFinishFailed(EditContext editContext)
-        {
-            Console.WriteLine($"Failed: {JsonConvert.SerializeObject(_vdew)}");
-
-        }
-
-        public void ChangeVDEW()
-        {
-
-        }
     }
 }
